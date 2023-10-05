cd1cb24b94a688056ad19e3effc0de5f3640c5aa
jrosebr <jrosebr@iu.edu>
2023-10-04 23:15:02 -0400

    Updated Brick tween bug

--- Brick/Brick.gd
+++ Brick/Brick.gd
@@ -48,12 +48,12 @@ func hit(_ball):
 func die():
 	dying = true
 	collision_layer = 0
-	$ColorRect.hide()
 	Global.update_score(score)
 	if not Global.feverish:
 		Global.update_fever(score)
 	get_parent().check_level()
 	$Confetti.emitting = true
+	
 	if randf() < powerup_prob:
 		var Powerup_Container = get_node_or_null("/root/Game/Powerup_Container")
 		if Powerup_Container != null:
