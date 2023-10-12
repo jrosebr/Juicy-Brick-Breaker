2f7d2bb9657cf57f88e2d6001f782a4d8918b625
jrosebr <jrosebr@iu.edu>
2023-10-11 21:47:59 -0400

    Finished 1.0 of my Project

--- Paddle/Paddle.gd
+++ Paddle/Paddle.gd
@@ -5,6 +5,7 @@ var speed = 10.0
 var width = 0
 var time_highlight = 0.4
 var time_highlight_size = 0.3
+var time_bounce = 0.75
 var tween
 
 func _ready():
@@ -14,6 +15,21 @@ func _ready():
 func _physics_process(_delta):
 	target.x = clamp(target.x, 0, Global.VP.x - width)
 	position = target
+	
+	var ball_container = get_node_or_null("/root/Game/Ball_Container")
+	if ball_container != null and ball_container.get_child_count() > 0:
+		var ball = ball_container.get_child(0)
+		$Eye1/Pupil/Sprite.position.x = 7
+		$Eye2/Pupil/Sprite.position.x = 7
+		$Eye1/Pupil.look_at(ball.position)
+		$Eye2/Pupil.look_at(ball.position)
+		var d = ((($Mouth.global_position.y - ball.global_position.y)/Global.VP.y)-0.2)*2
+		d = clamp(d, -1, 1)
+		$Mouth.scale.y = d
+	else:
+		$Eye1/Pupil/Sprite.position.x = 0
+		$Eye2/Pupil/Sprite.position.x = 0
+		$Mouth.scale.y = 1
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -28,9 +44,11 @@ func hit(_ball):
 		tween.kill()
 	tween = create_tween().set_parallel(true)
 	$Images/Highlight.modulate.a = 1.0
-	tween.tween_property($Images/Highlight, "modulate:a", 0, time_highlight)
-	$Images/Highlight.scale = Vector2(1.5, 1.5)
-	tween.tween_property($Images/Highlight, "scale", Vector2(1, 1), time_highlight_size).set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_IN)
+	tween.tween_property($Images/Highlight, "modulate:a", 0.0, time_highlight).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
+	$Images/Highlight.scale = Vector2(2.0,2.0)
+	tween.tween_property($Images/Highlight, "scale", Vector2(1.0,1.0), time_highlight_size).set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_IN)
+	$Images/Sprite2D.position.y = 30
+	tween.tween_property($Images/Sprite2D, "position:y", 10, time_bounce).set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT)
 
 
 #func powerup(payload):
